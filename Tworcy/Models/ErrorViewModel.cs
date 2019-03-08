c56efca31219506ba19bc05d3bece4bed692730f
Grzegorz <gpolomicz@gmail.com>
2019-03-08 12:29:08 +0100

    Dodaj pliki projektów.

---
+++ Tworcy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tworcy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
